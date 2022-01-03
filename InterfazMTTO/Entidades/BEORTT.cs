6efcf017292f29e0a981fd00703ba8f965000a8a
Maloko <marlon1428@gmail.com>
2022-01-03 16:09:30 -0500

    cambio de prueba

--- InterfazMTTO/Entidades/BEORTT.cs
+++ InterfazMTTO/Entidades/BEORTT.cs
@@ -6,7 +6,6 @@ namespace InterfazMTTO.iSBO_BE
         private DateTime rateDate;
         private string currency;
         private decimal rate;
-
         public DateTime RateDate { get => rateDate; set => rateDate = value; }
         public string Currency { get => currency; set => currency = value; }
         public decimal Rate { get => rate; set => rate = value; }
